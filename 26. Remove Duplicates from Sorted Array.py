a22e15b7a7ab0a305e1f88c595a43b17b565727a
anirudha <anirudha.kalita@datazenengineering.com>
2022-12-10 12:49:55 +0530

    Added the solution.

--- 26. Remove Duplicates from Sorted Array.py
+++ 26. Remove Duplicates from Sorted Array.py
@@ -1,10 +1,16 @@
-'''Given an integer array nums sorted in non-decreasing order, remove the duplicates in-place such that each unique element appears only once. The relative order of the elements should be kept the same.
+"""
+Given an integer array nums sorted in non-decreasing order, remove the duplicates
+ in-place such that each unique element appears only once. The relative order of the elements should be kept the same.
 
-Since it is impossible to change the length of the array in some languages, you must instead have the result be placed in the first part of the array nums. More formally, if there are k elements after removing the duplicates, then the first k elements of nums should hold the final result. It does not matter what you leave beyond the first k elements.
+Since it is impossible to change the length of the array in some languages, you must instead have the result be placed
+ in the first part of the array nums. More formally, if there are k elements after removing the duplicates,
+ then the first k elements of nums should hold the final result. It does not matter what you leave beyond the
+ first k elements.
 
 Return k after placing the final result in the first k slots of nums.
 
-Do not allocate extra space for another array. You must do this by modifying the input array in-place with O(1) extra memory.
+Do not allocate extra space for another array. You must do this by modifying
+the input array in-place with O(1) extra memory.
 
 Custom Judge:
 
@@ -19,5 +25,34 @@ assert k == expectedNums.length;
 for (int i = 0; i < k; i++) {
     assert nums[i] == expectedNums[i];
 }
-If all assertions pass, then your solution will be accepted.'''
+If all assertions pass, then your solution will be accepted.
+"""
 
+
+class Solution(object):
+    def removeDuplicates(self, nums):
+        """
+        :input: nums: List[int]
+        :rtype: int , List[int]
+        """
+        if not nums:
+            # checking empty array
+            return 0
+
+        initial_index = 0
+
+        for i in range(1, len(nums)):
+            if nums[initial_index] != nums[i]:
+                initial_index += 1
+                nums[initial_index] = nums[i]
+
+        return initial_index + 1
+
+
+obj = Solution()
+inputArray = [0, 0, 1, 1, 1, 2, 2, 3, 3, 4]
+output = obj.removeDuplicates(inputArray)
+print(output)
+
+# output = 5
+# The input list is also changed to, num = [0, 1, 2, 3, 4, 2, 2, 3, 3, 4]
